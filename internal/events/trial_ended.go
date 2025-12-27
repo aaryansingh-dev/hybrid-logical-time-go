072e968b79cb374ba087c98b8b9cabddc39283c3
Aaryan Singh <aaryan.jsingh@gmail.com>
2025-12-26 22:20:26 -0700

    Create trial_ended.go

---
+++ internal/events/trial_ended.go
@@ -0,0 +1,34 @@
+package events
+
+import (
+    "fmt"
+    "time"
+
+    "github.com/aaryansingh-dev/hybrid-logical-time-go/internal/clock"
+    "github.com/aaryansingh-dev/hybrid-logical-time-go/internal/engine"
+)
+
+type TrialEnded struct {
+    at time.Time
+	name string
+    customer string
+}
+
+func NewTrialEnded(at time.Time, customer string) *TrialEnded {
+    return &TrialEnded{at: at, customer: customer}
+}
+
+func (e *TrialEnded) Name() string{
+	return e.name
+}
+
+func (e *TrialEnded) Time() time.Time {
+    return e.at
+}
+
+func (e *TrialEnded) Execute(timeProvider clock.TimeProvider) []engine.Event {
+    fmt.Printf("[%s] Trial ended for %s\n", timeProvider.Now().Format(time.RFC3339), e.customer)
+
+    // Schedule invoice
+    return []engine.Event{}
+}
