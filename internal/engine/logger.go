ec6ec39cd5c7e13fd94f904a62b53706eaab6e02
Aaryan Singh <aaryan.jsingh@gmail.com>
2025-12-27 17:14:50 -0700

    create logger.go

---
+++ internal/engine/logger.go
@@ -0,0 +1,34 @@
+package engine
+
+import "time"
+import "fmt"
+import "strings"
+
+// Diagnostic defines the hooks for system observability.
+// This allows the engine to remain agnostic of how logs are formatted.
+type Diagnostic interface {
+	OnAdvanceStart(id string, start, target time.Time)
+	OnEventExecute(id string, eventName string, t time.Time)
+	OnEventCreated(id string, eventName string, t time.Time)
+	OnAdvanceFinish(id string, current time.Time)
+}
+
+// ConsoleLogger is one specific implementation of the Diagnostic interface.
+type ConsoleLogger struct{}
+
+func (c *ConsoleLogger) OnAdvanceStart(id string, start, target time.Time) {
+	fmt.Printf("\n[START]  %-12s | Advancing from %s -> %s\n", id, start.Format("15:04:05"), target.Format("15:04:05"))
+	fmt.Println(strings.Repeat("-", 80))
+}
+
+func (c *ConsoleLogger) OnEventExecute(id string, eventName string, t time.Time) {
+	fmt.Printf("[%s] EXEC  | %-12s | Event: %s\n", t.Format("15:04:05.000"), id, eventName)
+}
+
+func (c *ConsoleLogger) OnEventCreated(id string, eventName string, t time.Time) {
+	fmt.Printf("[%s] CHAIN | %-12s | Created: %s\n", t.Format("15:04:05.000"), id, eventName)
+}
+
+func (c *ConsoleLogger) OnAdvanceFinish(id string, current time.Time) {
+	fmt.Printf("[FINISH] %-12s | Simulation paused at %s\n", id, current.Format("15:04:05"))
+}
