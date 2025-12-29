f981fe9e2b34c023df0cdc1ab770f8cd9d4bdf9f
Aaryan Singh <aaryan.jsingh@gmail.com>
2025-12-28 20:06:17 -0700

    feat: made EventQueue resources goroutine safe

--- internal/engine/queue.go
+++ internal/engine/queue.go
@@ -1,29 +1,33 @@
 package engine
 
-import "container/heap"
+import (
+	"container/heap"
+	"sync"
+)
 
-type EventQueue struct{
-	events []Event	// simply a slice which will be turned into a heap
+type EventQueue struct {
+	events []Event    // simply a slice which will be turned into a heap
+	mu     sync.Mutex // not read heavy in comparison to writes, so using Mutex instead of RWMutex
 }
 
-func (q EventQueue) Len() int{
+func (q *EventQueue) Len() int {
 	return len(q.events)
 }
 
-func (q EventQueue) Less(i, j int) bool{
+func (q *EventQueue) Less(i, j int) bool {
 	return q.events[i].Time().Before(q.events[j].Time())
 }
 
-func (q EventQueue) Swap(i, j int){
+func (q *EventQueue) Swap(i, j int) {
 	q.events[i], q.events[j] = q.events[j], q.events[i]
 }
 
-func (q *EventQueue) Push(x interface{}){
-	event := x.(Event)		// converting this back to event. Heap expects and interface{} type i.e. any datatype. But, we need to tell the compiler that we know that the x is Event.
+func (q *EventQueue) Push(x interface{}) {
+	event := x.(Event) // converting this back to event. Heap expects and interface{} type i.e. any datatype. But, we need to tell the compiler that we know that the x is Event.
 	q.events = append(q.events, event)
 }
 
-func (q *EventQueue) Pop() interface{}{
+func (q *EventQueue) Pop() interface{} {
 	n := len(q.events)
 	event := q.events[n-1]
 	q.events = q.events[:n-1]
@@ -33,25 +37,33 @@ func (q *EventQueue) Pop() interface{}{
 // helpers to be made
 
 func NewEventQueue() *EventQueue {
-    q := &EventQueue{
-        events: []Event{},
-    }
-    heap.Init(q)
-    return q
+	q := &EventQueue{
+		events: []Event{},
+	}
+	heap.Init(q)
+	return q
 }
 
 func (q *EventQueue) PushEvent(e Event) {
-    heap.Push(q, e)
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	heap.Push(q, e)
 }
 
 func (q *EventQueue) PopEvent() Event {
-    return heap.Pop(q).(Event)
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	return heap.Pop(q).(Event)
 }
 
 func (q *EventQueue) Peek() Event {
-    if len(q.events) == 0 {
-        return nil
-    }
-    return q.events[0]
-}
+	q.mu.Lock()
+	defer q.mu.Unlock()
 
+	if len(q.events) == 0 {
+		return nil
+	}
+	return q.events[0]
+}
